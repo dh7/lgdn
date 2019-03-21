e7d40e205da9845bd5ce5475f03e7b4235f8a348
Damien Henry <dh7777777@gmail.com>
2019-03-21 21:48:13 +0000

    update script for latest data

---
+++ download.sh
@@ -0,0 +1,5 @@
+wget http://opendata.auth-6f31f706db6f4a24b55f42a6a79c5086.storage.sbg5.cloud.ovh.net/2019-03-21/LA_TRANSITION_ECOLOGIQUE.csv
+wget http://opendata.auth-6f31f706db6f4a24b55f42a6a79c5086.storage.sbg5.cloud.ovh.net/2019-03-21/LA_FISCALITE_ET_LES_DEPENSES_PUBLIQUES.csv
+wget http://opendata.auth-6f31f706db6f4a24b55f42a6a79c5086.storage.sbg5.cloud.ovh.net/2019-03-21/DEMOCRATIE_ET_CITOYENNETE.csv
+wget http://opendata.auth-6f31f706db6f4a24b55f42a6a79c5086.storage.sbg5.cloud.ovh.net/2019-03-21/ORGANISATION_DE_LETAT_ET_DES_SERVICES_PUBLICS.csv
+wget http://opendata.auth-6f31f706db6f4a24b55f42a6a79c5086.storage.sbg5.cloud.ovh.net/2019-03-21/EVENTS.csv
