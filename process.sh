07fb63372596661fd12cd3ffa68b883be4af2274
Damien Henry <dh7777777@gmail.com>
2019-03-22 21:01:09 +0000

    remove bad import

--- process.sh
+++ process.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 for i in {0..15}
 do
-  python process.py --filename LA_TRANSITION_ECOLOGIQUE.csv --prefix ECO --q_id $i --limit -1
+  python process.py --filename LA_TRANSITION_ECOLOGIQUE.csv --prefix ECO --q_id $i --limit 10000
 done
 
 for i in {0..7}
 do
-  python process.py --filename LA_FISCALITE_ET_LES_DEPENSES_PUBLIQUES.csv --prefix FIS --q_id $i --limit -1
+  python process.py --filename LA_FISCALITE_ET_LES_DEPENSES_PUBLIQUES.csv --prefix FIS --q_id $i --limit 10000
 done
 
 for i in {0..36}
 do
-  python process.py --filename DEMOCRATIE_ET_CITOYENNETE.csv  --prefix DEM --q_id $i --limit -1
+  python process.py --filename DEMOCRATIE_ET_CITOYENNETE.csv  --prefix DEM --q_id $i --limit 10000
 done
 
 for i in {0..32}
 do
-  python process.py --filename ORGANISATION_DE_LETAT_ET_DES_SERVICES_PUBLICS.csv  --prefix ORG --q_id $i --limit -1
+  python process.py --filename ORGANISATION_DE_LETAT_ET_DES_SERVICES_PUBLICS.csv  --prefix ORG --q_id $i --limit 10000
 done
 
 
