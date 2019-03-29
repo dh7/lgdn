8cb42f6ff164db4f30014856a902a6db51e3ac74
damien henry <dh@dh7.net>
2019-03-29 23:54:38 +0100

    fix questions creation

--- create_questions.py
+++ create_questions.py
@@ -1,36 +1,33 @@
 import json
-
-def byteify(input):
-    if isinstance(input, dict):
-        return {byteify(key): byteify(value)
-    for key, value in input.iteritems()}
-        elif isinstance(input, list):
-                    return [byteify(element) for element in input]
-        elif isinstance(input, unicode):
-                    return input.encode('utf-8')
-        else:
-                    return input
-
-with open('ECO_00/questions.json') as json_file:
-    ECO = json.load(json_file)
-with open('FIS_00/questions.json') as json_file:
-    FIS = json.load(json_file)
-with open('ORG_00/questions.json') as json_file:
-    ORG = json.load(json_file)
-with open('DEM_00/questions.json') as json_file:
-    DEM = json.load(json_file)
-
-html = u'<h2>La transition &eacute;cologique</h2>'
-for i, q in enumerate(ECO):
-    print(i, ECO[i],ECO[i][39])
-    q_id = u'ECO_{:02d}'.format(i)
-    a = u'<a href="https://storage.googleapis.com/lgdn/dataviz.html?prefix=' + q_id + u'"' + \
-	u'target="dataviz" onclick="closeNav()">' + \
-        ECO[i] + u'</a>\n'
-    html += a
-print (html)
-print (html[189])
 import codecs
+
+def create_question(prefix):
+    with open('{}_00/questions.json'.format(prefix),'rt', encoding="ascii") as json_file:
+        questions = json.load(json_file)
+    for i, q in enumerate(questions):
+        print(i, questions[i])
+        q_id = u'{}_{:02d}'.format(prefix, i)
+        a = u'<li><a href="https://storage.googleapis.com/lgdn-eu/dataviz.html?prefix=' + q_id + u'"\n' + \
+        u' target="dataviz" onclick="closeNav(this)")>\n' + \
+            questions[i] + u'\n</a></li>\n'
+        global html
+        html += a
+
+html = u'<h2>La transition &eacute;cologique</h2><ul>'
+create_question('ECO')
+html += '</ul>'
+
+html += u'<h2>La fiscalité et les dépenses publiques</h2><ul>'
+create_question('FIS')
+html += '</ul>'
+
+html += u'<h2>La démocratie et la citoyenneté</h2><ul>'
+create_question('DEM')
+html += '</ul>'
+
+html += u"<h2>L'organisation de l'État et des services publics</h2><ul>"
+create_question('ORG')
+html += '</ul>'
 
 with codecs.open('questions.html','w','utf8') as file:
     file.write(html)
